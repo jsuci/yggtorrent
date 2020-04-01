efb615a696e434dcf552898a663813d99d1bbf32
jsuci <jsuci.jsuci@gmail.com>
2020-04-01 21:12:41 +0800

    fix few bugs, no more user input, run the script and wait

--- yggtorrent_scraper.py
+++ yggtorrent_scraper.py
@@ -49,6 +49,7 @@ def process_entries(entries, index_url_str):
     p = ParseURL(index_url_str)
     data_path = Path(p.domain + ".json")
     data = []
+    sleep_time = 10
 
     if len(p.qs["page"][0]) == 0:
         page = 0
@@ -65,6 +66,8 @@ def process_entries(entries, index_url_str):
                 data = load(fi)
             except Exception:
                 data = []
+
+    names = [x["name"] for x in data]
 
     for e in tqdm(entries):
         d = dict()
@@ -101,32 +104,55 @@ def process_entries(entries, index_url_str):
 
         e_path = Path(name)
 
-        if d in data or e_path.exists():
-            if d not in data:
+        if d["name"] in names or e_path.exists():
+            if d["name"] not in names:
                 # print("Adding entry.")
                 data.append(d)
                 with open(data_path, "w", encoding="utf-8") as fo:
                     dump(data, fo, ensure_ascii=False)
-            else:
-                # print("Entry exists.")
-                pass
 
             if not e_path.exists():
                 # print("Exporting page.")
                 get_page(link, name)
+                sleep_time = 10
             else:
-                # print("Page exists.")
-                pass
+                sleep_time = 1
+
         else:
-            # print("Adding entry.")
             data.append(d)
             with open(data_path, "w", encoding="utf-8") as fo:
                 dump(data, fo, ensure_ascii=False)
 
-            # print("Exporting page.")
             get_page(link, name)
+            sleep_time = 10
 
-        sleep(5)
+        sleep(sleep_time)
+
+
+def get_page(url_str, name_str):
+
+    u = ParseURL(url_str)
+    f_name = Path(name_str)
+
+    if f_name.exists():
+        with open(f_name, "rb") as f:
+            doc = f.read()
+
+    if not f_name.exists() or len(doc) == 0:
+        ua = UserAgent()
+        cf = Session()
+        retries = Retry(total=10, backoff_factor=1)
+        headers = {"User-Agent": ua.random}
+        cf.mount(u.parse.scheme, HTTPAdapter(max_retries=retries))
+        cfs = create_scraper(sess=cf)
+        r = cfs.get(url_str, headers=headers)
+
+        doc = r.content
+
+        with open(f_name, "wb") as f:
+            f.write(doc)
+
+    return doc
 
 
 def next_page(url_str):
@@ -144,47 +170,25 @@ def next_page(url_str):
         rel_next_url = rel_obj[0].get("href")
         return (html, rel_next_url)
     else:
-        return None
-
-
-def get_page(url_str, name_str):
-
-    u = ParseURL(url_str)
-    f_name = Path(name_str)
-
-    if f_name.exists():
-        with open(f_name, "rb") as f:
-            doc = f.read()
-
-    if not f_name.exists() or len(doc) == 0:
-        ua = UserAgent()
-        cf = Session()
-        retries = Retry(total=5, backoff_factor=1)
-        headers = {"User-Agent": ua.random}
-        cf.mount(u.parse.scheme, HTTPAdapter(max_retries=retries))
-        r = cf.get(url_str, headers=headers)
-
-        doc = r.content
-
-        with open(f_name, "wb") as f:
-            f.write(doc)
-
-    return doc
+        return (None, None)
 
 
 def get_posts():
+    f_path = Path("error.log")
 
-    # Fetch index
-    # index = ("https://www2.yggtorrent.se/engine/search?"
-    #          "name=&description=&file=&uploader=&category=2144"
-    #          "&sub_category=all&do=search&attempt=1&page=200")
+    if f_path.exists():
+        with open(f_path, "r", encoding="utf-8") as fi:
+            for entry in fi:
+                if "://" in entry:
+                    index = entry.strip()
+                    print(f"Processing {index}")
+    else:
+        index = input("Enter index / page url: ")
 
-    index = input("Enter index / page url: ")
     html_index, rel_next = next_page(index)
 
     try:
         while rel_next:
-
             entry_index = html_index.select(".results > table > tbody > tr")
             process_entries(entry_index, index)
 
